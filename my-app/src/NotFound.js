19fa9a37b83b86f67daf3e4bd8ccc9762ce61c8c
Tomas Matukas <tomas.matukas@gmail.com>
2025-03-18 22:01:30 +0200

    updt

--- my-app/src/NotFound.js
+++ my-app/src/NotFound.js
@@ -3,7 +3,7 @@ import "./App.css";
 function NotFound() {
   return (
     <div className="container">
-      <h1>❌ Puslapis nerastas</h1>
+      <h1> Puslapis nerastas</h1>
       <p>Atrodo, kad šis puslapis neegzistuoja.</p>
       <a href="/" style={{ textDecoration: "none", color: "blue" }}>
         Grįžti į pagrindinį puslapį

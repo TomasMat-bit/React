b5a04949a9a6cda9bcdda25fe12a02d5fcb203f8
Tomas Matukas <tomas.matukas@gmail.com>
2025-03-18 20:20:13 +0200

    37dienos, 36 temos teorija

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,180 +1,137 @@
-// import React, { useEffect, useState} from 'react'
+// Example 3
+// import { BrowserRouter, Routes, Route, useParams, Link } from 'react-router-dom'
 
-// function Tema() {
-//   const [tema, setTema] = useState('light')
+// function OrderDetails() {
+//   let { userId, orderId } = useParams()
 
-//   useEffect(() => {
-//     document.body.className = tema
-//   }, [tema])
+//   return <h2>Vartotojo ID: {userId}, Uzsakymo ID: {orderId}</h2>
+// }
 
+// function Home() {
+//   return <h2>Home</h2>
+// }
+
+// function NotFound() {
 //   return (
 //     <div>
-//       <h2>Dabartine tema: {tema === 'light' ? 'Sviesi' : 'Tamsi'}</h2>
-//       <button onClick={() => setTema(tema === 'light' ? 'dark' : 'light')}>
-//         Keisti tema
-//       </button>
+//       <h2>404 - Puslapis nerastas</h2>
+//       <Link to='/'>Grizti i pagrindini puslapi</Link>
 //     </div>
 //   )
 // }
-
-// export default function App() {
-//   return <Tema />
-// }
-
-// // // // // // // // // // // // // // // // // // // // //
-// import React, { useEffect, useState } from 'react'
-
-// function Pranesimas() {
-//   const [rodyti, setRodyti] = useState(true)
-
-//   useEffect(() => {
-//     const timeris = setTimeout(() => {
-//       setRodyti(false)
-//     }, 10000)
-
-//     return () => clearTimeout(timeris)
-//   }, [])
-
-//   return (
-//     <div>
-//       {rodyti ? <h2>Sis pranesimas dings po 5 sekundziu</h2> : <h2>Pranesymas isnyko</h2>}
-//     </div>
-//   )
-// }
-
-// export default function App() {
-//   return <Pranesimas />
-// }
-
-// // // // // // // // // // // // // // // // // // // // //
-// import React, { useState, useEffect } from "react"
-
-// function SlinkimoStebejimas() {
-//   const [pozicija, setPozicija] = useState(0)
-
-//   useEffect(() => {
-//     const handeScroll = () => {
-//       setPozicija(window.scrollY)
-//     }
-
-//     window.addEventListener('scroll', handeScroll)
-//     console.log('1')
-
-//     return () => {
-//       window.removeEventListener('scroll', handeScroll)
-//       console.log('2')
-//     }
-//   }, [])
-
-//   return (
-//     <div style={{ heigh: "200vh", padding: "20 px"}}>
-//       <h2>Slinkimo pozicija {pozicija}</h2>
-//       <p>Slankite zemyn, kad pamatytumete pokycius.</p>
-//     </div>
-//   )
-// }
-
-// export default function App() {
-//   return (
-//       <SlinkimoStebejimas />
-//   )
-// }
-
-// // // // // // // // // // // // // // // // // // // // //
-// import React, { useEffect, useState } from "react";
-
-// function PavadinimoKeitimas() {
-//   const [skaicius, setSkaicius] = useState(0);
-
-//   useEffect(() => {
-//     document.title = `Paspaudimai ${skaicius}`
-//   }, [skaicius])
-
-//   return (
-//     <div>
-//       <h2>Pavadinimas keiciasi</h2>
-//       <button onClick={() => setSkaicius(skaicius + 1)}>+1</button>
-//     </div>
-//   )
-// }
-
-// export default function App() {
-//   return <PavadinimoKeitimas />
-// }
-
-// // // // // // // // // // // // // // // // // // // // // // //
-// import React, {useEffect} from "react"
-
-// function Montavimas() {
-//   useEffect(() => {
-//     console.log('Komponentas sumontuotas')
-//   }, [])
-
-//   return <h2>Komponentas umontuotas</h2>
-// }
-
-// export default function App() {
-//   return (
-//     <div>
-//       <Montavimas />
-//     </div>
-//   )
-// }
-
-// --------------------------------- UZDUOTYS 1 IR 2 --------------------------------
-
-// import React from "react";
-// import Skaiciuoklis from "./Skaiciuoklis";
-// import Zinute from "./Zinute";
 
 // function App() {
 //   return (
-//     <div>
-//       <Skaiciuoklis />
-//       <Zinute />
-//     </div>
-//   );
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home />}/>
+//         <Route path='/user/:userId/order/:orderId' element={<OrderDetails />}/>
+
+//         <Route path='*' element={<NotFound />} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
 // }
 
-// export default App;
+// export default App
 
-// --------------------------------------------------------------------------------------
+// Example 3
+// import { BrowserRouter, Routes, Route, useParams, Link } from 'react-router-dom'
 
-import React, { useEffect, useState } from "react";
+// function UserProfile() {
+//   let { id } = useParams()
 
-function Produktai() {
-  const [produktai, setProduktai] = useState([]);
+//   return <h2>Vartotojo ID: { id }</h2>
+// }
 
-  useEffect(() => {
-    fetch("http://localhost:5000/api/produktai")
-      .then((response) => response.json())
-      .then((data) => setProduktai(data))
-      .catch((error) => console.error("⚠️ Klaida gaunant duomenis:", error));
-  }, []);
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Pagrindinis puslapis</h2>
+//       <Link to='/user/1'>Vartotojas 1</Link>
+//       <br/>
+//       <Link to='/user/2'>Vartotojas 2</Link>
+//     </div>
+//   )
+// }
 
-  return (
-    <div>
-      <h2>Produktų sąrašas</h2>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Produkto pavadinimas</th>
-            <th>Kaina (€)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {produktai.map((produktas) => (
-            <tr key={produktas.id}>
-              <td>{produktas.id}</td>
-              <td>{produktas.produkto_pavadinimas}</td>
-              <td>{produktas.kaina.toFixed(2)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home />}/>
+//         <Route path='/user/:id' element={<UserProfile />} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
 
-export default Produktai;
+// export default App
+
+// Example 2
+// import { BrowserRouter, Routes,  Route, Link, NavLink } from 'react-router-dom'
+
+// function Navbar() {
+//   return (
+//     <nav>
+//       <Link to='/' style={{ margin: "0px 20px" }}>Pagrindinis</Link>
+//       <Link to='/about'>Apie mus</Link>
+//       <br/>
+
+//       <NavLink to='/' className={({isActive}) => (isActive ? 'active-link': '')}>
+//         Pagrindinis
+//       </NavLink>
+
+//       <NavLink to='/about' className={({isActive}) => (isActive ? 'active-link': '')}>
+//         Apie mus
+//       </NavLink>
+//     </nav>
+//   )
+// }
+
+// function Home() {
+//   return <h2>Home</h2>
+// }
+
+// function About() {
+//   return <h2>About</h2>
+// }
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+
+//       <Routes>
+//         <Route path='/' element={<Home />}></Route>
+//         <Route path='/about' element={<About />}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+
+// Example 1
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// function Home() {
+//   return <h2>Pagrindinis puslapis</h2>
+// }
+
+// function About() {
+//   return <h2>Apie mus</h2>
+// }
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home />}></Route>
+//         <Route path='/about' element={<About />}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
